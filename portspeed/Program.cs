00c9a52e9ca90cbd95edd3bb650e5a2dca3d6ddf
James Frost <james@frost-family.org>
2024-11-02 11:35:15 +0000

    created background worker to more effeciently read continuosly from the RNG

--- portspeed/Program.cs
+++ portspeed/Program.cs
@@ -3,6 +3,9 @@ using System;
 using System.Collections.Concurrent;
 using System.Diagnostics;
 using System.IO.Ports;
+using System.ComponentModel;  // for BackgroundWorker
+using Console = System.Console;
+
 
 namespace TrueRNGRanger
 {
@@ -19,99 +22,28 @@ namespace TrueRNGRanger
         public static ConcurrentStack<byte> _die12stack = new ConcurrentStack<byte>();
         public static ConcurrentStack<byte> _die20stack = new ConcurrentStack<byte>();
         public static ConcurrentStack<byte> _die100stack = new ConcurrentStack<byte>();
-        public void RunConcurrentStackSample()
-        {
-            FillUpCoinFlip(1000);
-            Task readerOne = Task.Run(() => GetFromStack());
-            Task readerTwo = Task.Run(() => GetFromStack());
-            Task readerThree = Task.Run(() => GetFromStack());
-            Task readerFour = Task.Run(() => GetFromStack());
-            Task.WaitAll(readerOne, readerTwo, readerThree, readerFour);
-        }
-
-        static void FillUpCoinFlip(int max)
-        {
-
-            //_coinstack.Clear();
-
-            SerialPort port = new SerialPort("COM4");
-            port.DtrEnable = true;
-            Boolean good = false;
-            uint trycount = 0;
-            while (good == false)
-            {
-                try
-                {
-                    port.Open();
-                    good = true;
-                }
-                catch { trycount++; }
-            } //This prevents a crash if this code is ran simultaniously in another instance
+        public static ConcurrentStack<byte> _randomBytes = new ConcurrentStack<byte>();
 
 
-            byte[] buffer = new byte[16];
 
-            bool flip = false;
-            for (int i = 0; i < max; i++)
-            {
-                int byread = port.Read(buffer, 0, 16);
-                for (int j = 0; j < byread; j++)
-                {
-                    byte readDataByte = (byte)buffer[j];
-                    flip = (readDataByte & 0x01) == 0x01;
-                    _coinstack.Push(flip);
-                    flip = (readDataByte & 0x02) == 0x02;
-                    _coinstack.Push(flip);
-                    flip = (readDataByte & 0x04) == 0x04;
-                    _coinstack.Push(flip);
-                    flip = (readDataByte & 0x08) == 0x08;
-                    _coinstack.Push(flip);
-                    flip = (readDataByte & 0x10) == 0x10;
-                    _coinstack.Push(flip);
-                    flip = (readDataByte & 0x20) == 0x20;
-                    _coinstack.Push(flip);
-                    flip = (readDataByte & 0x40) == 0x40;
-                    _coinstack.Push(flip);
-                    flip = (readDataByte & 0x80) == 0x80;
-                    _coinstack.Push(flip);
-                }
-            }
-
-            port.Close();
-
-        }
         static void FillUpDice(int die, int max)
         {
-
-            //_coinstack.Clear();
-
-            SerialPort port = new SerialPort("COM4");
-            port.DtrEnable = true;
-            Boolean good = false;
-            uint trycount = 0;
-            while (good == false)
-            {
-                try
-                {
-                    port.Open();
-                    good = true;
-                }
-                catch { trycount++; }
-            } //This prevents a crash if this code is ran simultaniously in another instance
-
-
-            byte[] buffer = new byte[16];
-
-
+            // This method converts from the concurrant byte stack into rolls / flips of the relevant die type in as efficient a manner as possible. It pushes the results onto a concurrent queue of the relevant type.
+            int readDataByte;
+            byte readByte;
+            bool success;
             for (int i = 0; i < max; i++)
             {
-                int byread = port.Read(buffer, 0, 16);
-                for (int j = 0; j < byread; j++)
+
+                success = _randomBytes.TryPop(out readByte);
+
+                if (success)
+
+                {
+                    readDataByte = (int)readByte + 1;
+                    if (die == 2)
                     {
-                        int readDataByte = (int)buffer[j] + 1;
-                        if (die == 2)
-                        {
-                         bool   flip = (readDataByte & 0x01) == 0x01;
+                        bool flip = (readDataByte & 0x01) == 0x01;
                         _coinstack.Push(flip);
                         flip = (readDataByte & 0x02) == 0x02;
                         _coinstack.Push(flip);
@@ -131,13 +63,9 @@ namespace TrueRNGRanger
 
                     if (die == 4)
                     {
-
                         _die4stack.Push((byte)((readDataByte % 4) + 1));
                         _die4stack.Push((byte)(((readDataByte / 4) % 4) + 1));
                         _die4stack.Push((byte)(((readDataByte / 64) % 4) + 1));
-
-
-
                     }
                     if (die == 6)
                     {
@@ -160,25 +88,21 @@ namespace TrueRNGRanger
                         {
                             _die8stack.Push((byte)((readDataByte % 8) + 1));
                             _die8stack.Push((byte)(((readDataByte / 8) % 8) + 1));
-
                         }
                         else if (readDataByte <= 128)
                         {
                             _die8stack.Push((byte)(((readDataByte - 64) % 8) + 1));
                             _die8stack.Push((byte)((((readDataByte - 64) / 8) % 8) + 1));
-
                         }
                         else if (readDataByte <= 192)
                         {
                             _die8stack.Push((byte)(((readDataByte - 128) % 8) + 1));
                             _die8stack.Push((byte)((((readDataByte - 128) / 8) % 8) + 1));
-
                         }
                         else if (readDataByte <= 256)
                         {
                             _die8stack.Push((byte)(((readDataByte - 192) % 8) + 1));
                             _die8stack.Push((byte)((((readDataByte - 192) / 8) % 8) + 1));
-
                         }
 
                     }
@@ -188,7 +112,6 @@ namespace TrueRNGRanger
                         {
                             _die10stack.Push((byte)((readDataByte % 10) + 1));
                             _die10stack.Push((byte)(((readDataByte / 10) % 10) + 1));
-
                         }
                         else if (readDataByte <= 200)
                         {
@@ -223,6 +146,10 @@ namespace TrueRNGRanger
                         {
                             _die12stack.Push((byte)((readDataByte % 12) + 1));
                             _die12stack.Push((byte)(((readDataByte / 12) % 12 + 1)));
+                        }
+                        else if (readDataByte <= 252)
+                        {
+                            _die12stack.Push((byte)(((readDataByte - 144) % 12) + 1));
 
                         }
 
@@ -245,14 +172,13 @@ namespace TrueRNGRanger
 
                     }
                 }
+                else
+                {
+                    //just skip and wait for the random byte stack to be populated by the background process
+                    i--;
+                }
             }
-
-            port.Close();
-
         }
-
-
-
 
         static int RollDice(int die, int count)
         {
@@ -274,7 +200,7 @@ namespace TrueRNGRanger
                     }
                     else
                     {
-                        FillUpDice(2, 10000);
+                        FillUpDice(2, 100);
                         i--;
                     }
                 }
@@ -292,7 +218,7 @@ namespace TrueRNGRanger
                     }
                     else
                     {
-                        FillUpDice(4, 10000);
+                        FillUpDice(4, 100);
                         i--;
                     }
                 }
@@ -308,7 +234,7 @@ namespace TrueRNGRanger
                     }
                     else
                     {
-                        FillUpDice(6, 10000);
+                        FillUpDice(6, 100);
                         i--;
                     }
                 }
@@ -324,7 +250,7 @@ namespace TrueRNGRanger
                     }
                     else
                     {
-                        FillUpDice(8, 10000);
+                        FillUpDice(8, 100);
                         i--;
                     }
                 }
@@ -340,7 +266,7 @@ namespace TrueRNGRanger
                     }
                     else
                     {
-                        FillUpDice(10, 1000);
+                        FillUpDice(10, 100);
                         i--;
                     }
                 }
@@ -356,7 +282,7 @@ namespace TrueRNGRanger
                     }
                     else
                     {
-                        FillUpDice(12, 10000);
+                        FillUpDice(12, 100);
                         i--;
                     }
                 }
@@ -372,7 +298,7 @@ namespace TrueRNGRanger
                     }
                     else
                     {
-                        FillUpDice(20, 10000);
+                        FillUpDice(20, 100);
                         i--;
                     }
                 }
@@ -389,7 +315,7 @@ namespace TrueRNGRanger
                     }
                     else
                     {
-                        FillUpDice(100, 1000);
+                        FillUpDice(100, 100);
                         i--;
                     }
                 }
@@ -398,21 +324,111 @@ namespace TrueRNGRanger
             return totalroll;
         }
 
-        static void GetFromStack()
+        static void worker_ProgressChanged(object _, ProgressChangedEventArgs e)
         {
-            bool res;
-            bool success = _coinstack.TryPop(out res);
-            while (success)
+            Console.WriteLine("Buffer size: {0:d}", e.ProgressPercentage);
+        }
+
+        static void worker_streamRandomBytes(object sender, DoWorkEventArgs e)
+        {
+            string strPort = "COM4"; //COM port your RNG is connected to. Should really move this to a config setting. Set to NONE if you want to use internal Pseudo Random number generator for testing purpose.
+            int numBytesToRead = 16; //Number of bytes to try and read at once from the RNG. Depending on your RNG, this setting could be worth tweaking (up/down) to see if it impacts performance.
+            int bufferSize = 10000000; //Size of buffer needed. Higher values will use more memory, but the buffer is generally way faster than reading off the RNG so if your workload is peaky having a large buffer being constantly updated can have a big (positive) impact on performance
+            Random rand = new Random(); //only needed if using NONE above.
+
+            Console.WriteLine("Worker: Starting to connect to " + strPort + "...\n");
+            BackgroundWorker? worker = sender as BackgroundWorker;
+            if (worker != null)
             {
-                Debug.WriteLine(res);
-                success = _coinstack.TryPop(out res);
+                SerialPort port = new SerialPort(strPort);
+                port.DtrEnable = true;
+                Boolean portOpen = false;
+                uint trycount = 0;
+                byte[] buffer = new byte[16];
+                int byread = numBytesToRead;
+
+                if (strPort != "NONE")
+                {
+                    while (portOpen == false)
+                    {
+                        try
+                        {
+                            port.Open();
+                            portOpen = true;
+                        }
+                        catch
+                        {
+                            Console.WriteLine("Worker: Failed to connect to " + strPort + "...\n");
+                            trycount++;
+                        }
+                    } //This prevents a crash if this port doesnt open.
+                }
+                e.Result = (long)0;
+
+                while (!worker.CancellationPending)
+                {
+                    if (_randomBytes.Count < bufferSize)
+                    {
+                        e.Result=(long)e.Result+1;
+                        for (int i = 0; i < 100000; i++)
+                        {
+                            if (strPort == "NONE")
+                            {
+                                rand.NextBytes(buffer);
+                            }
+                            else
+                            {
+                                byread = port.Read(buffer, 0, 16);
+                            }
+                            for (int j = 0; j < byread; j++)
+                            {
+                                byte readDataByte = buffer[j];
+                                _randomBytes.Push(readDataByte);
+                            }
+
+                            if (worker.CancellationPending)
+                                break;
+                        }
+                        worker.ReportProgress((int)_randomBytes.Count);
+                    }
+                }
+
+                port.Close();
+                e.Cancel = worker.CancellationPending;
             }
         }
 
+
+
+
+
+        static void worker_RunWorkerCompleted(object _, RunWorkerCompletedEventArgs e)
+        {
+            if (e.Cancelled)
+            {
+                Console.WriteLine("Worker: Shutting Down!");
+                Console.WriteLine("Worker: I worked {0:D} times.", e.Result);
+                return;
+            }
+
+            Console.WriteLine("Worker: I worked {0:D} times.", e.Result);
+            Console.WriteLine("Worker: Done now!");
+        }
+
+
         static void Main(string[] args)
         {
+            BackgroundWorker worker = new();
+            worker.DoWork += worker_streamRandomBytes;
+            worker.RunWorkerCompleted += worker_RunWorkerCompleted;
+            worker.ProgressChanged += worker_ProgressChanged;
+            worker.WorkerReportsProgress = true;
+            worker.WorkerSupportsCancellation = true;
 
-            bool flip = false;
+            worker.RunWorkerAsync();
+
+            //Console.ReadKey(true);  // event loop
+
             var stopwatch = new Stopwatch();
             long[] results = new long[101];
             double[] perf = new double[101];
@@ -420,8 +436,6 @@ namespace TrueRNGRanger
 
             //int diefaces = 6;
             int numdie = 1;
-            int maxChar = 0;
-            int Char = 0;
             int[] supportedDie = { 2, 4, 6, 8, 10, 12, 20, 100 };
 
             foreach (int diefaces in supportedDie)
@@ -431,7 +445,8 @@ namespace TrueRNGRanger
                 stopwatch.Reset();
                 stopwatch.Start();
                 Console.Write("Starting....");
-                for ( i = 0; i < 10000000; i++)
+
+                for (i = 0; i < 1000000; i++)
                 {
 
                     int rol = RollDice(diefaces, numdie);
@@ -445,16 +460,10 @@ namespace TrueRNGRanger
                     {
                         double freq = (double)i / (double)results[j];
                         double var = freq - (double)diefaces;
-                        double per = 1 / freq;
-                        double rat = per * (double)(diefaces ^ numdie);
 
-                        Console.WriteLine(j.ToString() + ": " + freq.ToString() +"   --  "+ var.ToString() );
+                        Console.WriteLine(j.ToString() + ": " + results[j].ToString() + " rolls. Frequency 1:" + freq.ToString() + "   --  Variance " + var.ToString());
 
                     }
-
-
-                    //Console.WriteLine("Heads: " + h.ToString());
-                    //Console.WriteLine("Tails: " + t.ToString());
 
                 }
 
@@ -462,18 +471,26 @@ namespace TrueRNGRanger
                 Console.WriteLine("chars: " + i.ToString());
                 perf[diefaces] = ((i) / (elap / 1000.0));
                 Console.WriteLine("rate: " + ((i) / (elap / 1000.0)).ToString() + " rolls per second");
+
             }
             foreach (int diefaces in supportedDie)
             {
-                Console.WriteLine("die: " + diefaces.ToString()+ "    -  "+ perf[diefaces] + " rolls per second");
+                Console.WriteLine("die: " + diefaces.ToString() + "    -  " + perf[diefaces] + " rolls per second");
             }
-                //Console.WriteLine("Heads: " + h.ToString());
-                //Console.WriteLine("Tails: " + t.ToString());
 
 
-
+            if (worker.IsBusy)
+            {
+                Console.WriteLine("Stopping the worker...");
+                worker.CancelAsync();
+                var sw = System.Diagnostics.Stopwatch.StartNew();
+                while (worker.IsBusy && sw.ElapsedMilliseconds < 5000)
+                    System.Threading.Thread.Sleep(1);
 
             }
+
+
+        }
 
     }
 }
